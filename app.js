919dcf739cc139934c571d7e73d873ef07fbf066
Jiahao Li <144624616+jiahaoli0465@users.noreply.github.com>
2023-09-17 20:33:32 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const h1 = document.querySelector('h1');
+function randomRGB(){
+    const r = Math.floor( Math.random()*256);
+    const g = Math.floor( Math.random()*256);
+    const b = Math.floor( Math.random()*256);
+    return `rgb(${r},${g},${b})`
+}
+
+// h1.style.color = randomRGB();
+// setInterval(function(){
+//     h1.style.color = randomRGB();
+// }, 500)
+
+const letters = document.querySelectorAll('.letter');
+
+setInterval(function(){
+for (let letter of letters) {
+    letter.style.color = randomRGB();
+}
+
+},500)
+
